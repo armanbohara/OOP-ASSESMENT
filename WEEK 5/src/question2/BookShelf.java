c9566ac7221bd421136b2f79de4065c7fa302b15
armanbohara <110483111+armanbohara@users.noreply.github.com>
2025-05-02 21:25:02 +0545

    Update BookShelf.java

--- WEEK 5/src/question2/BookShelf.java
+++ WEEK 5/src/question2/BookShelf.java
@@ -2,8 +2,8 @@ package question2;
 
 public class BookShelf {
     public static void main(String[] args) {
-        Book book1 = new Book("OOP", "Dibas Mainali", "hello", "2025");
-        Book book2 = new Book("Computer communucation", "Mainali Dibas", "CC book", "2026");
+        Book book1 = new Book("The Pursuit of Happiness", "Arman", "Bohara Publication", "23-2-1990");
+        Book book2 = new Book("Harry Potter", "Arman", "Bohara Publication", "23-6-1991");
         
         System.out.println(book1);
         System.out.println();
