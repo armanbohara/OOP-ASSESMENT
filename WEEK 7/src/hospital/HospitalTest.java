a05bed16490f1a5f4d28f4cf9c63590f8bc3b2c6
armanbohara <110483111+armanbohara@users.noreply.github.com>
2025-05-02 21:47:10 +0545

    Update HospitalTest.java

--- WEEK 7/src/hospital/HospitalTest.java
+++ WEEK 7/src/hospital/HospitalTest.java
@@ -3,10 +3,10 @@ package hospital;
 //Driver class
 public class HospitalTest {
  public static void main(String[] args) {
-     Doctor doc = new Doctor(1, "Dr. Dibas");
-     Nurse nurse = new Nurse(2, "bibas");
-     Receptionist receptionist = new Receptionist(3, "digant");
-     Cleaner cleaner = new Cleaner(4, "ram");
+     Doctor doc = new Doctor(1, "Dr. Arman");
+     Nurse nurse = new Nurse(2, "AB");
+     Receptionist receptionist = new Receptionist(3, "CD");
+     Cleaner cleaner = new Cleaner(4, "EF");
 
      doc.diagnose();
      nurse.assist();
