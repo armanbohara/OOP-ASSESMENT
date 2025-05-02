ea7103f4fca1d418fc39bd178bef65de133201c7
armanbohara <110483111+armanbohara@users.noreply.github.com>
2025-05-02 21:32:04 +0545

    Update Transactions.java

--- WEEK 5/src/question4/Transactions.java
+++ WEEK 5/src/question4/Transactions.java
@@ -2,10 +2,10 @@ package question4;
 
 public class Transactions {
     public static void main(String[] args) {
-        Account acc1 = new Account("dibas", 11221122, 1000);
-        Account acc2 = new Account("Digant",21231321);
+        Account acc1 = new Account("Arman", 835253838, 55000);
+
         
-        acc2.deposit(500);
+        acc2.deposit(55000);
         System.out.println(acc1);
         System.out.println(acc2);
     }
