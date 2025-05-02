197f1b29cba5d32de382727cdaef725546c99d31
armanbohara <110483111+armanbohara@users.noreply.github.com>
2025-05-02 08:51:50 +0545

    Update PersonalDetails.java

--- WEEK 1/src/Weekly_assignment/PersonalDetails.java
+++ WEEK 1/src/Weekly_assignment/PersonalDetails.java
@@ -2,9 +2,9 @@ package Weekly_assignment;
 
 public class PersonalDetails {
     public static void main(String[] args) {
-        System.out.println("Name: Dibas Mainali");
+        System.out.println("Name: Arman Bohara");
         System.out.println("Address: Kathmandu, Nepal");
-        System.out.println("Age: 22");
-        System.out.println("Phone Number: 9818482978");
+        System.out.println("Age: 20");
+        System.out.println("Phone Number: 9845386375");
     }
 }
